33f082f0eada3014d962f95413d694ad99e9caf0
Jacob Freeman <t00niyf5@duck.com>
2024-09-08 21:17:56 -0400

    Tests coming along

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo $1
+set -e
+
+echo "Testing random ISBN 1000 times."
+for (( i = 0; i < 1000; i++ )); do
+    rand=$($1 -r)
+    $1 -c "$rand" || (echo "Rand failed with ISBN: $rand" && false)
+    echo "ISBN: $rand Passed."
+done
+
