70136d413dd3bc3461e79af9a6d4e5be496faf0c
Jacob Freeman <t00niyf5@duck.com>
2024-09-08 20:09:36 -0400

    Add support for ISBN-13

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 
 void help(char* prog);
 void fix_isbn(const char *isbn10);
-void check_and_remove(const char *isbn, char* isbnout, unsigned char minimum);
-unsigned short sum_isbn(const char* isbn, const unsigned char to);
+void check_and_remove(const char *isbn, char* isbnout, bool full);
+unsigned short sum_isbn(const char* isbn, bool full, bool is13);
 void check_isbn(const char *isbn);
 void random_isbn(void);
 
@@ -32,28 +32,29 @@ int main(const int argc, char *argv[])
                 help(argv[0]);
                 exit(EXIT_SUCCESS);
             case 'f':
-                printf("fixing: %s\n", optarg);
+                fprintf(stderr, "fixing: %s\n", optarg);
                 fix_isbn(optarg);
                 break;
             case 'c':
-                printf("Checking ISBN: %s\n", optarg);
+                fprintf(stderr, "Checking ISBN: %s\n", optarg);
                 check_isbn(optarg);
                 break;
             case 'r':
                 random_isbn();
                 break;
             case '?':
-                printf("unknown option: %c\n", optopt);
-            break;
+                fprintf(stderr, "unknown option: %c\n", optopt);
+                break;
             default:
-                printf("unknown error.");
+                fprintf(stderr, "unknown error.");
+                exit(EXIT_FAILURE);
         }
     }
     return 0;
 }
 
 void help(char *prog) {
-    printf("Usage: %s [options]\nOptions:\n\t-c <isbn>\tCheck an ISBN 10\n\t-r\t\t\tCreate a random ISBN 10\n\t-f <isbn>\tCorrect an invalid ISBN 10\n\t-h\t\t\tShow this help.\n", prog);
+    fprintf(stderr, "Usage: %s [options]\nOptions:\n\t-c <isbn>\tCheck an ISBN 10\n\t-r\t\t\tCreate a random ISBN 10\n\t-f <isbn>\tCorrect an invalid ISBN 10\n\t-h\t\t\tShow this help.\n", prog);
 }
 
 void random_isbn(void){
@@ -75,12 +76,19 @@ srand(time(NULL));
 }
 
 void check_isbn(const char *isbn) {
-    char newisbn[13];
-    check_and_remove(isbn, newisbn, 10);
+    char newisbn[17];
+    bool is13;
+    check_and_remove(isbn, newisbn, true);
 
-    unsigned short final = sum_isbn(newisbn, 10);
+    if (strlen(newisbn) == 10 || strlen(newisbn) == 9) {
+        is13 = false;
+    } else {
+        is13 = true;
+    }
 
-    if (final == 11) {
+    const unsigned short final = sum_isbn(newisbn, 10, is13);
+
+    if ((is13 && final == 10) || (!is13 && final == 11)) {
         printf("The ISBN %s is Valid.\n", isbn);
     } else {
         printf("The ISBN %s is not Valid. Run with -f to fix it.\n", isbn);
@@ -89,19 +97,36 @@ void check_isbn(const char *isbn) {
 }
 
 void fix_isbn(const char *isbn10) {
-    char newisbn[13];
-    check_and_remove(isbn10, newisbn, 9);
+    char newisbn[17];
+    bool is13;
+    check_and_remove(isbn10, newisbn, false);
 
-    unsigned short final = sum_isbn(newisbn, 9);
+    if (strlen(newisbn) == 10 || strlen(newisbn) == 9) {
+        is13 = false;
+    } else {
+        is13 = true;
+    }
+
+    unsigned short final = sum_isbn(newisbn, false, is13);
 
     if (final == 11) {
         final = 0;
     }
-    for (int i = 0; i < 11; i++) {
-        if (i == 1 || i == 4 || i == 10) {
-            printf("-");
+    if (is13 && final == 10) {
+        final = 0;
+    }
+    const int top  = is13 ? 13:10;
+    for (int i = 0; i < top; i++) {
+        if (!is13) {
+            if (i == 1 || i == 4 || i == 9) {
+                printf("-");
+            }
+        } else {
+            if (i == 3 || i == 4 || i == 9 || i == 12) {
+                printf("-");
+            }
         }
-        if (i < 10) {
+        if (i < top-1) {
             printf("%c", newisbn[i]);
         } else {
             if (final != 10) {
@@ -114,11 +139,33 @@ void fix_isbn(const char *isbn10) {
 }
 
 
-unsigned short sum_isbn(const char* isbn, const unsigned char to) {
+unsigned short sum_isbn(const char* isbn, const bool full, const bool is13) {
     unsigned short sum = 0;
 
     char digit[2];
     digit[1] = '\0';
+    if (is13) {
+        for (int i = 0; i < strlen(isbn); i++) {
+            digit[0] = isbn[i];
+            int currdigit;
+            if (digit[0] == 'x' || digit[0] == 'X') {
+                currdigit = 10;
+            } else {
+                currdigit = atoi(digit);
+            }
+            if (i < (full?13:12)) {
+                unsigned char weight;
+                if (i%2 == 0) {
+                    weight = 1;
+                } else {
+                    weight = 3;
+                }
+                sum += currdigit*weight;
+            }
+        }
+        return (unsigned short)10-sum%10;
+    }
+    // Else
     for (int i = 0,h=10; i < strlen(isbn); i++,h--) {
         digit[0] = isbn[i];
         int currdigit;
@@ -127,7 +174,7 @@ unsigned short sum_isbn(const char* isbn, const unsigned char to) {
         } else {
             currdigit = atoi(digit);
         }
-        if (i < to) {
+        if (i < (full?10:9)) {
             sum += currdigit*h;
         }
     }
@@ -136,14 +183,21 @@ unsigned short sum_isbn(const char* isbn, const unsigned char to) {
 
 void remove_non_digits(const char* isbn, char* isbnout);
 
-void check_and_remove(const char *isbn, char* isbnout, const unsigned char minimum) {
-    if (strlen(isbn) > 13) {
-        fprintf(stderr, "Invalid ISBN-10: %s\n(did you use too many hyphens?)\n", isbn);
+void check_and_remove(const char *isbn, char* isbnout, const bool full) {
+    if (strlen(isbn) > 17) {
+        fprintf(stderr, "Invalid ISBN: %s\n(did you use too many hyphens?)\n", isbn);
         exit(EXIT_FAILURE);
     }
     remove_non_digits(isbn, isbnout);
 
-    if (strlen(isbnout) < minimum) {
+    if (strlen(isbnout) != 13 && strlen(isbnout) != 12 && strlen(isbnout) != 10 && strlen(isbnout) != 9) {
+        fprintf(stderr, "Invalid ISBN.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Long check to make sure the ISBN is the right length
+    if (full && (((strlen(isbnout) == 13 || strlen(isbnout) == 12) && strlen(isbnout) == 12) ||
+        ((strlen(isbnout) == 10 || strlen(isbnout) == 9) && strlen(isbnout) == 9))) {
         fprintf(stderr, "Invalid ISBN.\n");
         exit(EXIT_FAILURE);
     }
